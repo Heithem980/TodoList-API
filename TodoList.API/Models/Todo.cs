824ae9f118d446f99067cd10ccc0e170562d4181
Heithem980 <heithem.gha98@gmail.com>
2024-08-20 11:17:59 +0200

    created ToDo model

---
+++ TodoList.API/Models/Todo.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoList.API.Models
+{
+    public class Todo
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        public string? Title { get; set; }
+        public bool isCompleted { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? CompletedAt { get; set; }
+
+
+
+
+    }
+}
